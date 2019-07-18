6f426b5536196e9d12b1a701542d2a9bb3abcbd7
Jisha Kambo <iamjishak@gmail.com>
2019-07-17 18:23:55 -0700

    playing around with data binding Assignment 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PropertyBindingType } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username: string = "test";
+
+  getUsername() {
+    return this.username;
+  }
 }
